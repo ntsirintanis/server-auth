8458a2732085cf7ff33f321ee65cf451d13c59f4
Nikos Tsirintanis <ntsirintanis@therp.nl>
2019-11-15 09:40:08 +0100

    Minimal version of partner_user_management for ppozo purposes

--- partner_user_management/models/res_users.py
+++ partner_user_management/models/res_users.py
@@ -16,7 +16,7 @@ class Resusers(models.Model):
         for actual user creation.
         """
         login = login or partner.email
-        template_user = template_user or self.get_template_user(partner)
+        template_user = template_user
         if partner.user_ids:
             raise exceptions.UserError(
                 _('Partner %s already has a user account') %
@@ -33,15 +33,3 @@ class Resusers(models.Model):
         if template_user:
             return template_user.sudo().copy(default=vals)
         return self.sudo().create(vals)
-
-    @api.model
-    def get_template_user(self, partner):
-        """Get user to use as template for creation.
-
-        It is very unlikely that this function will not return a template
-        user, but it might happen if the default users have been deleted.
-        """
-        return self.env.ref(
-            'auth_signup.default_template_user',
-            raise_if_not_found=False) or self.env.ref(
-                'base.default_user', raise_if_not_found=False) or False
